de7b84c69bc3ed370b6933289c38aa47d95f1d1e
Stefano Zanella <stefano.zanella@sponsorpay.com>
2016-01-24 16:24:27 +0100

    Implement sliding window loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
 using std::cout;
 using std::endl;
 
-using pcl::PointCloud;
-using pcl::PointXYZRGB;
-using pcl::VoxelGrid;
 using pcl::io::loadPCDFile;
-using pcl::visualization::PCLVisualizer;
-using pcl::visualization::PointCloudColorHandlerRGBField;
+
+typedef pcl::PointXYZRGB PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
 
 int main(int argc, char** argv) {
   if (argc < 2) {
@@ -20,41 +19,60 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  PointCloud<PointXYZRGB>::Ptr cloud_in (new PointCloud<PointXYZRGB>);
-  PointCloud<PointXYZRGB>::Ptr cloud_downsampled (new PointCloud<PointXYZRGB>);
+  PointCloudT::Ptr cloud (new PointCloudT);
 
-  if (loadPCDFile<PointXYZRGB>(argv[1], *cloud_in) == -1) {
+  if (loadPCDFile<PointT>(argv[1], *cloud) == -1) {
     PCL_ERROR("Couldn't read the pcd file.\n");
     return -1;
   }
 
-  cout << "Original point cloud: " << cloud_in->width << " x " << cloud_in->height << endl;
+  cout << "Original point cloud: " << cloud->width << " x " << cloud->height << endl;
+  cout << "Point cloud size: " << cloud->width * cloud->height << endl;
 
-  VoxelGrid<PointXYZRGB> downsampler;
-  downsampler.setInputCloud(cloud_in);
-  downsampler.setLeafSize(0.05f, 0.05f, 0.05f);
-  downsampler.filter(*cloud_downsampled);
+  int window_width = 64;
+  int window_height = 64;
+  int step_size = 64;
 
-  cout << "Downsampled point cloud: " << cloud_downsampled->width << " x " << cloud_downsampled->height << endl;
+  //int k = 30, j = 264; // Face 1 img 25
+  //int k = 100, j = 300; // Face 2 img 25
+  //int k = 80, j = 350; // Face 3 img 25
+  // int k = 70, j = 450; // Face 4 img 25
+  for (int k = 0; k < cloud->height - window_height; k += step_size) {
+    for (int j = 0; j < cloud->width - window_width; j += step_size) {
+      //int x_from = j, x_to = j + window_width, y_from = k, y_to = k + window_height;
+      //cout << "Extracting window (" << x_from << " - " << x_to << ") x ("<< y_from << " - " << y_to << ")" << endl;
 
-  PCLVisualizer viewer ("PCL Viewer");
-  int viewport;
-	viewer.createViewPort(0.0, 0.0, 1.0, 1.0, viewport);
-	viewer.setBackgroundColor(0, 0, 0);
-	viewer.addCoordinateSystem(0.1);
-	viewer.initCameraParameters();
+      PointCloudT::Ptr window (new PointCloudT);
 
-	viewer.addText("Original cloud", 10, 10, "viewport_text", viewport);
+      // TODO Why does it work while instead if I copy the range of points
+      // manually the colors are screwed up? I think the answer lies in either
+      // pcl::ExtractIndices.filter or pcl::PCLBase.setIndices.
+      // Solving this (i.e. being able to do this manually) would probably allow
+      // for a great speedup, as each new window can be built by removing a
+      // row/column and adding another one.
+      // Q: Will all this matter since most of the work is done via an integral
+      // image?
+      pcl::ExtractIndices<PointT> ei (false);
+      ei.setInputCloud(cloud);
+      ei.setIndices(k, j, window_height, window_width);
+      ei.setKeepOrganized(true);
+      ei.filter(*window);
 
-  viewer.addPointCloud<PointXYZRGB>(
-    cloud_downsampled,
-    PointCloudColorHandlerRGBField<PointXYZRGB>(cloud_downsampled),
-    "original",
-    viewport
-  );
+      pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+      viewer.setCameraPosition(0,0,-2,0,-1,0,0);
 
-  cout << "Visualizing..." << endl;
-  viewer.spin();
+      pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb(window);
+      pcl::visualization::PointCloudColorHandlerRGBField<PointT> rgb_c(cloud);
+
+      int left_vp, right_vp;
+      viewer.createViewPort(0.0, 0.0, 0.5, 1.0, left_vp);
+      viewer.createViewPort(0.5, 0.0, 1.0, 1.0, right_vp);
+
+      viewer.addPointCloud<PointT> (cloud, rgb_c, "cloud", left_vp);
+      viewer.addPointCloud<PointT> (window, rgb, "window", right_vp);
+      viewer.spin();
+    }
+  }
 
   return 0;
 }
