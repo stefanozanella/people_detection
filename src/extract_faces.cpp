535c096421bd8ad9cdef8de5cfb00b4e3ca1c006
Stefano Zanella <stefano.zanella@sponsorpay.com>
2016-02-01 14:24:08 +0100

    Save selected faces to disk.

--- src/extract_faces.cpp
+++ src/extract_faces.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/extract_indices.h>
@@ -6,6 +7,8 @@
 #include "window_selector.h"
 
 using pcl::io::loadPCDFile;
+using pcl::io::savePCDFileASCII;
+using std::ostringstream;
 
 int main(int argc, char** argv) {
   PointCloudT::Ptr cloud (new PointCloudT);
@@ -17,6 +20,22 @@ int main(int argc, char** argv) {
 
   WindowSelector selector (cloud);
   selector.spin();
+
+  ExtractIndices face_extractor (false);
+  face_extractor.setInputCloud(cloud);
+
+  for (int k = 0; k < selector.faces().size(); k++) {
+    PointCloudWindow faceCoords = selector.faces().at(k);
+
+    PointCloudT::Ptr face (new PointCloudT);
+
+    face_extractor.setIndices(faceCoords.y, faceCoords.x, faceCoords.size, faceCoords.size);
+    face_extractor.filter(*face);
+
+    ostringstream filename;
+    filename << "dataset/positive/" << k << ".pcd";
+    savePCDFileASCII(filename.str(), *face);
+  }
 
   return 0;
 }
