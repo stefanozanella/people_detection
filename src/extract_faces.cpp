cfde7a16b12e6a2f63f8473db7b55fb2be20750c
Stefano Zanella <stefano.zanella@sponsorpay.com>
2016-02-01 21:49:14 +0100

    Extract faces from all pcd files in input directory.

--- src/extract_faces.cpp
+++ src/extract_faces.cpp
@@ -15,6 +15,7 @@ using pcl::io::savePCDFileASCII;
 using std::ostringstream;
 using std::max;
 using std::string;
+using boost::filesystem::is_directory;
 using boost::filesystem::path;
 using boost::filesystem::directory_iterator;
 
@@ -31,24 +32,41 @@ int firstFreeSampleNumberIn(string dir) {
 }
 
 int main(int argc, char** argv) {
-  PointCloudT::Ptr cloud (new PointCloudT);
 
+  string input_dir (argv[1]);
   string output_dir (argv[2]);
 
-  if (loadPCDFile<PointT>(argv[1], *cloud) == -1) {
-    cout << "Couldn't load cloud file " << argv[1] << endl;
-    return -1;
-  }
+  directory_iterator pcd_file ((path(input_dir)));
+  directory_iterator end;
+  while (pcd_file != end) {
+    cout << pcd_file->path() << endl;
 
-  WindowSelector selector (cloud);
-  selector.spin();
+    PointCloudT::Ptr cloud (new PointCloudT);
 
-  for (int k = 0, sample_number = firstFreeSampleNumberIn("dataset/positive"); k < selector.faces().size(); k++, sample_number++) {
-    PointCloudT::Ptr face = selector.faces().at(k);
+    if (is_directory(pcd_file->path())) {
+      pcd_file++;
+      continue;
+    }
 
-    ostringstream filename;
-    filename << output_dir << sample_number << ".pcd";
-    savePCDFileASCII(filename.str(), *face);
+    if (loadPCDFile<PointT>(pcd_file->path().string(), *cloud) == -1) {
+      cout << "Couldn't load cloud file " << argv[1] << endl;
+      return -1;
+    }
+
+    WindowSelector selector (cloud);
+    selector.spin();
+
+    for (int k = 0, sample_number = firstFreeSampleNumberIn("dataset/positive"); k < selector.faces().size(); k++, sample_number++) {
+      PointCloudT::Ptr face = selector.faces().at(k);
+
+      ostringstream filename;
+      filename << output_dir << "/" << sample_number << ".pcd";
+      savePCDFileASCII(filename.str(), *face);
+    }
+
+    pcd_file++;
+
+    cout << "next" << endl;
   }
 
   return 0;
